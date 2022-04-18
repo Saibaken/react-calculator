90610e351b2d4c7e0ce89b7ca5cf9258fe7ef9a8
Saibaken <shelamov.s@gmail.com>
2022-04-18 23:21:07 +0300

    feat: :construction: Added form and stylesheets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <div className="container d-flex flex-column justify-content-center align-items-center">
+      <div className="row flex-column col-12 col-md-8 col-lg-4">
+        <div className="col faded-text my-1 text-end">Previous Value</div>
+        <input className="col border-0 main-text my-1 text-end w-100" type="text" placeholder="Current Input" />
+      </div>
+      <div className="row gap-1 d-flex col-12 col-md-8 col-lg-4 justify-content-center">
+          <div className="row gap-1 d-flex flex-row justify-content-center">
+            <button className="btn btn-x-operations rounded-pill col">sqrt</button>
+            <button className="btn btn-x-operations rounded-pill col">pi</button>
+            <button className="btn btn-x-operations rounded-pill col">^</button>
+            <button className="btn btn-x-operations rounded-pill col">!</button>
+          </div>
+
+          <div className="row gap-1 d-flex flex-row justify-content-around">
+            <button className="btn btn-ac         col round-button">AC</button>
+            <button className="btn btn-operations col round-button">()</button>
+            <button className="btn btn-operations col round-button">%</button>
+            <button className="btn btn-operations col round-button">/</button>
+          </div>
+
+          <div className="row gap-1 d-flex flex-row  justify-content-around">
+            <button className="btn btn-numbers btn-lg col round-button">7</button>
+            <button className="btn btn-numbers btn-lg col round-button">8</button>
+            <button className="btn btn-numbers btn-lg col round-button">9</button>
+            <button className="btn btn-operations btn-lg col round-button">*</button>
+          </div>
+
+          <div className="row gap-1 d-flex flex-row  justify-content-around">
+            <button className="btn btn-numbers btn-lg col round-button">4</button>
+            <button className="btn btn-numbers btn-lg col round-button">5</button>
+            <button className="btn btn-numbers btn-lg col round-button">6</button>
+            <button className="btn btn-operations btn-lg col round-button">-</button>
+          </div>
+
+          <div className="row gap-1 d-flex flex-row  justify-content-around">
+            <button className="btn btn-numbers btn-lg col round-button">1</button>
+            <button className="btn btn-numbers btn-lg col round-button">2</button>
+            <button className="btn btn-numbers btn-lg col round-button">3</button>
+            <button className="btn btn-operations btn-lg col round-button">+</button>
+          </div>
+
+          <div className="row gap-1 d-flex flex-row  justify-content-around">
+            <button className="btn btn-numbers btn-lg col round-button">0</button>
+            <button className="btn btn-operations btn-lg col round-button">,</button>
+            <button className="btn btn-operations btn-lg col round-button">&lt;-</button>
+            <button className="btn btn-equals btn-lg col round-button">=</button>
+          </div>
+      </div>
+      </div>
+    </form>
   );
 }
-
-export default App;
