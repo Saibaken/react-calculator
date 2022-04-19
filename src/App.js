6466f4bb54ddafe6272692d533e3618bf0a54e19
Saibaken <shelamov.s@gmail.com>
2022-04-19 21:29:06 +0300

    feat: :sparkles: Added simple logic for brackets

--- src/App.js
+++ src/App.js
@@ -19,21 +19,35 @@ export default function App() {
     setCurrentInput("");
   };
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const getAnswer = () => {
+    console.log("🚀 ~ file: App.js ~ line 25 ~ submitHandler ~ currentInput", currentInput)
     const answer = evaluate(currentInput);
     setCurrentInput(answer.toString());
   };
-
-  const precentClick = () => {};
-
-  const bracketClick = () => {};
-
+  
+  const precentClick = () => {
+    
+  };
+  
+  const bracketClick = () => {
+    const lastChar = currentInput.slice(-1);
+    if (["+","-","*","/","^"].includes(lastChar)) {
+      addToInput("(");
+    } else {
+      if (currentInput.split("(").length === currentInput.split(")").length) {
+        addToInput("(");
+      } else {
+        addToInput(")");
+      } 
+    }
+  };
+  
   const handleKeyboard = (e) => {
     console.log("🚀 ~ file: App.js ~ line 34 ~ handleKeyboard ~ e.key", e.key)
     switch (e.key) {
       case "Enter":
-        submitHandler(e);
+        console.log("🚀 ~ file: App.js ~ line 36 ~ handleKeyboard ~ currentInput", currentInput)
+        getAnswer();
         break;
       case "0":
       case "1":
@@ -49,6 +63,8 @@ export default function App() {
       case "-":
       case "*":
       case "/":
+      case "!":
+      case "^":
         addToInput(e.key);
         break;
       default:
@@ -57,28 +73,31 @@ export default function App() {
   };
 
   useEffect(() => {
-    document.addEventListener("keydown", handleKeyboard)
-  }, []);
+    window.addEventListener("keydown", handleKeyboard);
+    return () => {
+      window.removeEventListener("keydown", handleKeyboard);
+    }
+  });
   
 
   return (
-    <form onSubmit={(e) => submitHandler(e)}>
-      <div className="container d-flex flex-column justify-content-center align-items-center">
+    <form>
+      <div className="container gap-2 d-flex flex-column justify-content-center align-items-center">
       <div className="row flex-column col-12 col-md-8 col-lg-4">
         <div className="col previous-value my-1 text-end">Previous Value</div>
         <div className="col border-0 current-input my-1 text-end w-100">&nbsp;{currentInput}</div>
       </div>
       <div className="row gap-1 d-flex col-12 col-md-8 col-lg-4 justify-content-center">
           <div className="row gap-1 d-flex flex-row justify-content-center">
-            <button className="btn btn-x-operations rounded-pill col" type="button">√</button>
-            <button className="btn btn-x-operations rounded-pill col" type="button">π</button>
-            <button className="btn btn-x-operations rounded-pill col" type="button">^</button>
-            <button className="btn btn-x-operations rounded-pill col" type="button">!</button>
+            <button className="btn btn-x-operations rounded-pill h-75 col lh-sm pb-3" type="button" onClick={() => addToInput("sqrt(")}>√</button>
+            <button className="btn btn-x-operations rounded-pill h-75 col lh-sm pb-3" type="button" onClick={() => addToInput("pi")}>π</button>
+            <button className="btn btn-x-operations rounded-pill h-75 col lh-sm pb-3" type="button" onClick={() => addToInput("^")}>^</button>
+            <button className="btn btn-x-operations rounded-pill h-75 col lh-sm pb-3" type="button" onClick={() => addToInput("!")}>!</button>
           </div>
 
           <div className="row gap-1 d-flex flex-row justify-content-around">
             <button className="btn btn-ac         col rounded-circle" type="button" onClick={allClearClick}>AC</button>
-            <button className="btn btn-operations col rounded-circle" type="button" onClick={bracketClick}>()</button>
+            <button className="btn btn-operations col rounded-circle" type="button" onClick={bracketClick}>( )</button>
             <button className="btn btn-operations col rounded-circle" type="button" onClick={precentClick}>%</button>
             <button className="btn btn-operations col rounded-circle" type="button" onClick={() => addToInput("/")}>÷</button>
           </div>
@@ -108,7 +127,7 @@ export default function App() {
             <button className="btn btn-numbers col rounded-circle" type="button" onClick={() => addToInput("0")}>0</button>
             <button className="btn btn-operations col rounded-circle" type="button" onClick={() => addToInput(".")}>,</button>
             <button className="btn btn-operations col rounded-circle" type="button" onClick={removeClick}>←</button>
-            <button className="btn btn-equals col rounded-circle" type="submit">=</button>
+            <button className="btn btn-equals col rounded-circle" type="button" onClick={getAnswer}>=</button>
           </div>
       </div>
       </div>
